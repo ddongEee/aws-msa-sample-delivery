f28ef4cde0bc51977083a218cb02b7e3e297e07c
rhoeunju <ej.rho35@gmail.com>
2021-12-17 17:31:31 +0900

    updated DeliveryDummyRepository: added a DeliveryId - Delivery mapping

--- infrastructure/src/main/java/com/aws/peach/infrastructure/dummy/DeliveryDummyRepository.java
+++ infrastructure/src/main/java/com/aws/peach/infrastructure/dummy/DeliveryDummyRepository.java
@@ -1,36 +1,51 @@
 package com.aws.peach.infrastructure.dummy;
 
 import com.aws.peach.domain.delivery.Delivery;
+import com.aws.peach.domain.delivery.DeliveryId;
 import com.aws.peach.domain.delivery.DeliveryRepository;
 import com.aws.peach.domain.delivery.OrderNo;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Repository
 public class DeliveryDummyRepository implements DeliveryRepository {
 
-    private final Map<OrderNo, Delivery> map;
+    private final Map<OrderNo, Delivery> orderNo2Delivery;
+    private final Map<DeliveryId, Delivery> deliveryId2Delivery;
 
     public DeliveryDummyRepository() {
-        this.map = new ConcurrentHashMap<>();
+        this.orderNo2Delivery = new ConcurrentHashMap<>();
+        this.deliveryId2Delivery = new ConcurrentHashMap<>();
     }
 
-    DeliveryDummyRepository(Map<OrderNo, Delivery> map) {
-        this.map = new ConcurrentHashMap<>(map);
+    DeliveryDummyRepository(Collection<Delivery> initData) {
+        this.orderNo2Delivery = initData.stream()
+                .collect(Collectors.toMap(Delivery::getOrderNo, Function.identity()));
+        this.deliveryId2Delivery = initData.stream()
+                .collect(Collectors.toMap(Delivery::getId, Function.identity()));
+    }
+
+    @Override
+    public Optional<Delivery> findById(DeliveryId deliveryId) {
+        return Optional.ofNullable(this.deliveryId2Delivery.get(deliveryId));
     }
 
     @Override
     public Optional<Delivery> findByOrderNo(OrderNo orderNo) {
-        return Optional.ofNullable(map.get(orderNo));
+        return Optional.ofNullable(orderNo2Delivery.get(orderNo));
     }
 
     @Override
     public Delivery save(Delivery delivery) {
         delivery.assignNewId();
-        map.put(delivery.getOrderNo(), delivery);
+        orderNo2Delivery.put(delivery.getOrderNo(), delivery);
+        deliveryId2Delivery.put(delivery.getId(), delivery);
         return delivery;
     }
 }
--- infrastructure/src/test/groovy/com/aws/peach/infrastructure/dummy/DeliveryDummyRepositoryTest.groovy
+++ infrastructure/src/test/groovy/com/aws/peach/infrastructure/dummy/DeliveryDummyRepositoryTest.groovy
@@ -1,6 +1,7 @@
 package com.aws.peach.infrastructure.dummy
 
 import com.aws.peach.domain.delivery.Delivery
+import com.aws.peach.domain.delivery.DeliveryId
 import com.aws.peach.domain.delivery.OrderNo
 import spock.lang.Specification
 
@@ -8,47 +9,70 @@ class DeliveryDummyRepositoryTest extends Specification {
 
     // test data
     OrderNo orderNo;
+    DeliveryId deliveryId;
     Delivery delivery;
 
     def setup() {
-        orderNo = new OrderNo("123")
-        delivery = Delivery.builder().orderNo(orderNo).build()
+        orderNo = new OrderNo("o123")
+        deliveryId = new DeliveryId("d123")
+        delivery = Delivery.builder().id(deliveryId).orderNo(orderNo).build()
     }
 
-    def "should save delivery item"() {
+    def "should save delivery"() {
         given:
         DeliveryDummyRepository repository = new DeliveryDummyRepository()
 
         when:
-        delivery = repository.save(delivery)
+        Delivery savedDelivery = repository.save(delivery)
 
         then:
-        delivery.getId() != null
-        delivery.getOrderNo() == new OrderNo("123")
-        repository.map.get(new OrderNo("123")) == delivery
+        repository.orderNo2Delivery.get(orderNo) == delivery
+        repository.deliveryId2Delivery.get(savedDelivery.getId()) == delivery
     }
 
-    def "should retrieve saved delivery item"() {
+    def "should find delivery by order_no"() {
         given:
-        Map<OrderNo, Delivery> map = new HashMap<>()
-        map.put(orderNo, delivery)
-        DeliveryDummyRepository repository = new DeliveryDummyRepository(map)
+        List<Delivery> initData = Collections.singletonList(delivery)
+        DeliveryDummyRepository repository = new DeliveryDummyRepository(initData)
 
         when:
         Delivery delivery1 = repository.findByOrderNo(orderNo).get()
 
         then:
-        delivery1.getOrderNo() == new OrderNo("123")
+        delivery1.getOrderNo() == orderNo
     }
 
-    def "when delivery not found, should return empty optional"() {
+    def "when delivery not found by order_no, should return empty optional"() {
         given:
-        Map<OrderNo, Delivery> map = new HashMap<>()
-        map.put(orderNo, delivery)
-        DeliveryDummyRepository repository = new DeliveryDummyRepository(map)
+        List<Delivery> initData = Collections.singletonList(delivery)
+        DeliveryDummyRepository repository = new DeliveryDummyRepository(initData)
 
         when:
-        Optional<Delivery> deliveryOpt = repository.findByOrderNo(new OrderNo("234"))
+        Optional<Delivery> deliveryOpt = repository.findByOrderNo(new OrderNo("o234"))
+
+        then:
+        deliveryOpt.isEmpty()
+    }
+
+    def "should find delivery by delivery_id"() {
+        given:
+        List<Delivery> initData = Collections.singletonList(delivery)
+        DeliveryDummyRepository repository = new DeliveryDummyRepository(initData)
+
+        when:
+        Delivery delivery1 = repository.findById(deliveryId).get()
+
+        then:
+        delivery1.getId() == deliveryId
+    }
+
+    def "when delivery not found by delivery_id, should return empty optional"() {
+        given:
+        List<Delivery> initData = Collections.singletonList(delivery)
+        DeliveryDummyRepository repository = new DeliveryDummyRepository(initData)
+
+        when:
+        Optional<Delivery> deliveryOpt = repository.findById(new DeliveryId("d234"))
 
         then:
         deliveryOpt.isEmpty()

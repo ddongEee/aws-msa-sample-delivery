72d855ed9486a2524f7373378db21ee00fcebe89
EJ Rho <ej.rho35@gmail.com>
2022-01-13 14:48:59 +0900

    moved query apis from DeliveryController to DeliveryQueryController:
    to limit source code file length

--- interface/src/main/java/com/aws/peach/interfaces/api/DeliveryController.java
+++ interface/src/main/java/com/aws/peach/interfaces/api/DeliveryController.java
@@ -1,11 +1,9 @@
 package com.aws.peach.interfaces.api;
 
-import com.aws.peach.application.DeliveryQueryService;
 import com.aws.peach.application.DeliveryService;
 import com.aws.peach.application.CreateDeliveryInput;
 import com.aws.peach.domain.delivery.Delivery;
 import com.aws.peach.domain.delivery.DeliveryId;
-import com.aws.peach.domain.delivery.OrderNo;
 import com.aws.peach.interfaces.common.JsonException;
 import com.aws.peach.interfaces.common.JsonUtil;
 import com.aws.peach.interfaces.common.ValidationException;
@@ -22,14 +20,10 @@ import javax.validation.Valid;
 public class DeliveryController {
 
     private final DeliveryService deliveryService;
-    private final DeliveryQueryService deliveryQueryService;
     private final JsonUtil jsonUtil;
 
-    public DeliveryController(DeliveryService deliveryService,
-                              DeliveryQueryService deliveryQueryService,
-                              JsonUtil jsonUtil) {
+    public DeliveryController(DeliveryService deliveryService, JsonUtil jsonUtil) {
         this.deliveryService = deliveryService;
-        this.deliveryQueryService = deliveryQueryService;
         this.jsonUtil = jsonUtil;
     }
 
@@ -45,24 +39,6 @@ public class DeliveryController {
         Delivery delivery = deliveryService.createDeliveryOrder(order);
         DeliveryDetailResponse response = DeliveryDetailResponse.of(delivery);
         return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/{deliveryId}")
-    public ResponseEntity<DeliveryDetailResponse> queryById(@PathVariable String deliveryId) {
-        log.info("GET /delivery/{}", deliveryId);
-        return deliveryQueryService.getDelivery(new DeliveryId(deliveryId))
-                    .map(DeliveryDetailResponse::of)
-                    .map(ResponseEntity::ok)
-                    .orElseGet(() -> ResponseEntity.ok().build());
-    }
-
-    @GetMapping
-    public ResponseEntity<DeliveryDetailResponse> queryByOrderNo(@RequestParam(name = "orderNo") String orderNo) {
-        log.info("GET /delivery?orderNo={}", orderNo);
-        return deliveryQueryService.getDelivery(new OrderNo(orderNo))
-                .map(DeliveryDetailResponse::of)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.ok().build());
     }
 
     @PutMapping("/{deliveryId}/prepare")
---
+++ interface/src/main/java/com/aws/peach/interfaces/api/DeliveryQueryController.java
@@ -0,0 +1,37 @@
+package com.aws.peach.interfaces.api;
+
+import com.aws.peach.application.DeliveryQueryService;
+import com.aws.peach.domain.delivery.DeliveryId;
+import com.aws.peach.domain.delivery.OrderNo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/delivery")
+public class DeliveryQueryController {
+    private final DeliveryQueryService deliveryQueryService;
+
+    public DeliveryQueryController(DeliveryQueryService deliveryQueryService) {
+        this.deliveryQueryService = deliveryQueryService;
+    }
+
+    @GetMapping("/{deliveryId}")
+    public ResponseEntity<DeliveryDetailResponse> queryById(@PathVariable String deliveryId) {
+        log.info("GET /delivery/{}", deliveryId);
+        return deliveryQueryService.getDelivery(new DeliveryId(deliveryId))
+                .map(DeliveryDetailResponse::of)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.ok().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<DeliveryDetailResponse> queryByOrderNo(@RequestParam(name = "orderNo") String orderNo) {
+        log.info("GET /delivery?orderNo={}", orderNo);
+        return deliveryQueryService.getDelivery(new OrderNo(orderNo))
+                .map(DeliveryDetailResponse::of)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.ok().build());
+    }
+}

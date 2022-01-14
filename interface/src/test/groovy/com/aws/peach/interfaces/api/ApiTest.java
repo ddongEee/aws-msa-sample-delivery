187159b817845c3e64c622cb3832964bcf14a58e
EJ Rho <ej.rho35@gmail.com>
2022-01-14 10:22:22 +0900

    updated annotation ApiTest: removed @DirtiesContext

--- interface/src/test/groovy/com/aws/peach/interfaces/api/ApiTest.java
+++ interface/src/test/groovy/com/aws/peach/interfaces/api/ApiTest.java
@@ -2,7 +2,6 @@ package com.aws.peach.interfaces.api;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.ElementType;
@@ -14,7 +13,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9192", "port=9192"})
 public @interface ApiTest {
 }

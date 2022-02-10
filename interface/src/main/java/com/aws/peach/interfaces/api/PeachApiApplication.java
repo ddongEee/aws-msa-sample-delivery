72c69cc88f65ab42ef3116d0e452ad8ed0a0d35a
EJ Rho <ej.rho35@gmail.com>
2022-02-10 22:39:54 +0900

    Updated PeachApiApplication @Import

--- interface/src/main/java/com/aws/peach/interfaces/api/PeachApiApplication.java
+++ interface/src/main/java/com/aws/peach/interfaces/api/PeachApiApplication.java
@@ -1,17 +1,13 @@
 package com.aws.peach.interfaces.api;
 
 import com.aws.peach.configuration.PeachApiContextConfig;
-import com.aws.peach.infrastructure.configuration.api.PeachApiInfrastructureContextConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
-@Import(value = {
-		PeachApiContextConfig.class,
-		PeachApiInfrastructureContextConfig.class,
-})
+@Import(value = {PeachApiContextConfig.class})
 public class PeachApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PeachApiApplication.class, args);
